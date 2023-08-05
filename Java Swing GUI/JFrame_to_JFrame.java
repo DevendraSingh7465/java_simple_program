35111b733a7edcf81f8062be665ca5ef7280aba7
Devendra Singh <127510525+DevendraSingh7465@users.noreply.github.com>
2023-08-05 14:15:00 +0530

    Update JFrame_to_JFrame.java

--- Java Swing GUI/JFrame_to_JFrame.java
+++ Java Swing GUI/JFrame_to_JFrame.java
@@ -1,1 +1,40 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
+class JFrame_to_JFrame{
+    public JFrame_to_JFrame(){}
+    public static void main(String[] args) {
+        JFrame_to_JFrame j = new JFrame_to_JFrame();
+        Add ad = new Add();
+    }
+}
+
+class Add extends JFrame{
+    public Add(){
+        super("Addition Form");
+        JButton b = new JButton("ADD FORM");
+        b.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae){
+                new Sub();
+                dispose();
+            }
+        });
+        add(b);
+        setLayout(new FlowLayout());
+        setVisible(true);
+        setSize(400, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// When app is closed then code will also stop
+    }
+
+}
+class Sub extends JFrame{
+    public Sub(){
+        super("Subtraction Form");   
+        setLayout(new FlowLayout());
+        setVisible(true);
+        setSize(400, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// When app is closed then code will also stop     
+    }
+
+}
